951fc36f8667194b79df8f5b9229223e32ccbd2b
WolfInSheepsSkin <ics22084@uom.edu.gr>
2023-05-13 19:02:46 +0300

    Fixed selected flash bug + character name bug

--- src/main/java/com/lovelaced/PlayerCustomizationController.java
+++ src/main/java/com/lovelaced/PlayerCustomizationController.java
@@ -3,23 +3,20 @@ package com.lovelaced;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.game.Game;
 import com.game.Player;
 import javafx.animation.TranslateTransition;
-import javafx.application.Preloader;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -27,63 +24,26 @@ import javafx.util.Duration;
 
 public class PlayerCustomizationController {
 
-    private HashMap<String,String> selectedColors = new HashMap<String,String>();
-    private HashMap<String,ImageView> selectedFlasks = new HashMap<String,ImageView>();
+    private HashMap<String,String> selectedColors = new HashMap<>();
+    private HashMap<String,ImageView> selectedFlasks = new HashMap<>();
 
-    private HashMap<String, String> selectedNameFields = new HashMap<String, String>();
+    private HashMap<String, String> selectedNameFields = new HashMap<>();
 
-    private HashMap<String, Image> selectedPlayers = new HashMap<String, Image>();
-
-    @FXML
-    private ImageView arrow1;
+    private HashMap<String, Image> selectedPlayers = new HashMap<>();
 
     @FXML
-    private ImageView arrow2;
+    private ImageView arrow1, arrow2, arrow3, arrow4;
 
     @FXML
-    private ImageView arrow3;
+    private ImageView exitButton, leftAddButton, rightAddButton;
 
     @FXML
-    private ImageView arrow4;
-
-
-    @FXML
-    private ImageView exitButton;
-
+    private AnchorPane pane1, pane2, pane3, pane4;
 
     @FXML
-    private ImageView leftAddButton;
+    private ImageView firstPlayer, secondPlayer, thirdPlayer, fourthPlayer;
 
-    @FXML
-    private AnchorPane pane3;
-
-    @FXML
-    private AnchorPane pane2;
-
-    @FXML
-    private AnchorPane pane1;
-
-
-    @FXML
-    private ImageView rightAddButton;
-
-    @FXML
-    private AnchorPane pane4;
-
-
-    @FXML
-    private ImageView firstPlayer;
-
-    @FXML
-    private ImageView secondPlayer;
-
-    @FXML
-    private ImageView thirdPlayer;
-
-    @FXML
-    private ImageView fourthPlayer;
     private Stage stage;
-    private Scene scene;
     private Parent root;
 
     Player createPlayer(String name, Image avatar, String colored) {
@@ -91,11 +51,11 @@ public class PlayerCustomizationController {
     }
 
     @FXML
-    void startGame(MouseEvent event) throws IOException {
+    public void startGame(MouseEvent event) throws IOException {
         Game.getPlayerList().clear();
-        String name = null;
-        String colored = null;
-        Image avatar = null;
+        String name;
+        String colored;
+        Image avatar;
         boolean playerCreationFilter = true;
 
         for ( String pane : selectedNameFields.keySet()) {
@@ -111,6 +71,7 @@ public class PlayerCustomizationController {
                 playerCreationFilter = false;
 
         }
+        for(Player p : Game.getPlayerList()) {System.out.println(p.getName());}
         if (Game.getPlayerList().size() >= 2 && playerCreationFilter) {
             root = FXMLLoader.load(getClass().getResource("GameScreen-view.fxml"));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -125,14 +86,14 @@ public class PlayerCustomizationController {
     }
 
     @FXML
-    void nameTyped(MouseEvent event) {
+    public void nameTyped(KeyEvent event) {
         TextField nameFiled = (TextField) event.getSource();
-        pane = nameFiled.getParent().getId();
+        String pane = nameFiled.getParent().getId();
         selectedNameFields.put(pane, nameFiled.getText());
     }
 
     @FXML
-    void exitClicked(MouseEvent event) throws IOException {
+    public void exitClicked(MouseEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("StartScreen-view.fxml"));
 
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
@@ -144,74 +105,59 @@ public class PlayerCustomizationController {
     }
 
     @FXML
-    void leftClicked(MouseEvent event) {
+    public void leftClicked(MouseEvent event) {
         pane3.setVisible(true);
     }
     @FXML
-    void rightClicked(MouseEvent event) {
+    public void rightClicked(MouseEvent event) {
         pane4.setVisible(true);
     }
     private ImageView arrow = new ImageView();
-    private String paneColor = null;
 
-    private String pane = null;
-    private String color = null;
     @FXML
-    void arrowClicked(MouseEvent event) throws FileNotFoundException {
-        if(paneColor != null)
-            selectedFlasks.get(paneColor).setStyle(null);
-        boolean exist = false;
-        ImageView flask =  (ImageView) event.getSource();
-        colorPicker(flask);
+    public void flaskClicked(MouseEvent event) throws FileNotFoundException {
+        ImageView flask = (ImageView) event.getSource();
+        String pane = flask.getParent().getId();
+        String color = colorPicker(flask);
+        if ( !selectedColors.containsValue(color) ) {
 
-        for ( String Tempcolor : selectedColors.keySet() ) {
-            if ( selectedColors.get( Tempcolor ).equals( color ) ) { exist = true; flask.setStyle( null ); break; }
-        }
+            selectedColors.put( pane, color );
 
-        if ( !exist ){
+            if( selectedFlasks.containsKey(pane) ){
+                selectedFlasks.get(pane).setStyle(null);
+            }
 
-            if(paneColor != null && !paneColor.equals(flask.getParent().getId()) && color != null)
-                selectedFlasks.get(paneColor).setStyle("-fx-effect: dropShadow(three-pass-box, " + selectedColors.get(paneColor) + ", 10, 0, 0, 0)");
+            selectedFlasks.put( pane, flask );
+            selectedFlasks.get(pane).setStyle("-fx-effect: dropShadow(three-pass-box, " + color + ", 30, 0, 0, 0)");
+            animateArrow(pane,selectedFlasks.get(pane),1000);
 
-            paneColor = flask.getParent().getId();
-
-            selectedColors.put( paneColor, color );
-
-            animateArrow(flask, 1000);
-
-            flask.setStyle("-fx-effect: dropShadow(three-pass-box, " + color + ", 30, 0, 0, 0)");
-
-            selectedFlasks.put( paneColor, flask );
-            flask = selectedFlasks.get(paneColor);
-
-            setPlayerImage(paneColor);
+            setPlayerImage(pane);
         }
 
     }
 
-    private void colorPicker(ImageView flask) {
+    public String colorPicker( ImageView flask) {
         String colorUrl = flask.getImage().getUrl();
+        int pathIndex = colorUrl.lastIndexOf("flask" );
+        String color = colorUrl.substring( pathIndex );
 
-        int temp = colorUrl.lastIndexOf("flask" );
-        color = colorUrl.substring( temp );
-
-        switch ( color ) {
-            case "flaskYellow.png": color = "yellow"; break;
-            case "flaskRed.png":    color = "red"; break;
-            case "flaskBlue.png":   color = "blue"; break;
-            case "flaskGreen.png":  color = "green"; break;
+        switch (color) {
+            case "flaskYellow.png" -> color = "yellow";
+            case "flaskRed.png" -> color = "red";
+            case "flaskBlue.png" -> color = "blue";
+            case "flaskGreen.png" -> color = "green";
         }
+        return color;
     }
 
-    private void animateArrow(ImageView flask, int duration) {
-
+    public void animateArrow(String pane, ImageView flask, int duration) {
         arrow.setVisible(false);
 
-        switch (paneColor) {
-            case "pane1": arrow = arrow1; break;
-            case "pane2": arrow = arrow2; break;
-            case "pane3": arrow = arrow3; break;
-            case "pane4": arrow = arrow4; break;
+        switch (pane) {
+            case "pane1" -> arrow = arrow1;
+            case "pane2" -> arrow = arrow2;
+            case "pane3" -> arrow = arrow3;
+            case "pane4" -> arrow = arrow4;
         }
 
         double numberY;
@@ -234,36 +180,33 @@ public class PlayerCustomizationController {
         translate.setAutoReverse(true);
         translate.setNode(arrow);
         translate.play();
+
     }
 
     public void setPlayerImage(String pane) throws FileNotFoundException {
         String path = "src/main/resources/assets/gameScreen/Character_Window_";
         switch (pane) {
-            case "pane1": {
+            case "pane1" -> {
                 firstPlayer.setImage(new Image(new FileInputStream(path + selectedColors.get(pane) + ".png")));
                 selectedPlayers.put(pane, firstPlayer.getImage());
-                break;
             }
-            case "pane2": {
+            case "pane2" -> {
                 secondPlayer.setImage(new Image(new FileInputStream(path + selectedColors.get(pane) + ".png")));
                 selectedPlayers.put(pane, secondPlayer.getImage());
-                break;
             }
-            case "pane3": {
+            case "pane3" -> {
                 thirdPlayer.setImage(new Image(new FileInputStream(path + selectedColors.get(pane) + ".png")));
                 selectedPlayers.put(pane, thirdPlayer.getImage());
-                break;
             }
-            case "pane4": {
+            case "pane4" -> {
                 fourthPlayer.setImage(new Image(new FileInputStream(path + selectedColors.get(pane) + ".png")));
                 selectedPlayers.put(pane, fourthPlayer.getImage());
-                break;
             }
         }
     }
 
     @FXML
-    void initialize() {
+    public void initialize() {
         assert arrow1 != null : "fx:id=\"arrow1\" was not injected: check your FXML file 'PlayerCustomizationScreen-view.fxml'.";
         assert arrow2 != null : "fx:id=\"arrow2\" was not injected: check your FXML file 'PlayerCustomizationScreen-view.fxml'.";
         assert arrow3 != null : "fx:id=\"arrow3\" was not injected: check your FXML file 'PlayerCustomizationScreen-view.fxml'.";
@@ -279,7 +222,6 @@ public class PlayerCustomizationController {
         assert rightAddButton != null : "fx:id=\"rightAddButton\" was not injected: check your FXML file 'PlayerCustomizationScreen-view.fxml'.";
         assert secondPlayer != null : "fx:id=\"secondPlayer\" was not injected: check your FXML file 'PlayerCustomizationScreen-view.fxml'.";
         assert thirdPlayer != null : "fx:id=\"thirdPlayer\" was not injected: check your FXML file 'PlayerCustomizationScreen-view.fxml'.";
-
     }
 
 }
